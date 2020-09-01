886a0e2b920bc22c2f56f2a093d8a2a5be59eece
Rishi Prajapati <leftyyyy@users.noreply.github.com>
2020-09-01 11:31:27 +0530

    CarFueling solution fixed, optimized solution as well as another optimized psuedo code, ChildrenCelebrationGroupPartition psuedo code added

---
+++ src/com/leftyyyy/childrenCelebrationProblem/ChildrenCelebrationGroupPartition.java
@@ -0,0 +1,16 @@
+package com.leftyyyy.childrenCelebrationProblem;
+
+public class ChildrenCelebrationGroupPartition {
+    /*
+    pusedo code
+     */
+    //make input array sorted
+    //R<-{}, i<-1
+    //while i<=n:
+    //  [l,r]<-[xi, xi + 1]
+    //  R<-R U { [l,r] }
+    //  i<-i+1
+    //  while i<=n and xi <= r :
+    //      i <- i + 1
+    //return R
+}
--- src/com/leftyyyy/greedy/CarFueling.java
+++ src/com/leftyyyy/greedy/CarFueling.java
@@ -1,58 +1,53 @@
 package com.leftyyyy.greedy;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CarFueling {
     public static int computeMinRefills(int dist, int tank, int[] stops) {
-        int count = 0;
-        ArrayList<Integer> stopsList = new ArrayList<>();
-        for(int t:stops) {
-            stopsList.add(t);
-        }
-        ArrayList<Integer> stopsLists =  (ArrayList<Integer>) stopsList.clone();
-        for (int item:stopsLists) {
-            if(item>=tank && item<=dist){
-                if(item==tank){
-                    int i = 0;
-                    while(stopsList.get(i)!=item){
-                        stopsList.remove(i);
-                    }
-                    count = count + 1;
-                    tank = tank + item;
-                }else{
-                    int i=0;
-                    while(i<stopsList.indexOf(item)-1){
-                        stopsList.remove(i);
-                    }
-                    count = count + 1;
-                    tank = tank + stopsList.get(stopsList.indexOf(item)-1);
-                }
+        //MinRefills(x,n,l)
+        //
+        //
+        //numRefill<-0, currentRefill<-0
+        //while(currentRefill<=n):
+        //  lastRefill<-currentRefill
+        //  while(currentRefill<=n and x[currentRefill+1] - x[lastRefill]<=L):
+        //      currentRefill<-currentRefill+1
+        //  if(currentRefill==lastRefill):
+        //      return -1;
+        //  if(currentRefill<=n):
+        //      numRefill<-numRefills+1
+        //return numRefills
+
+        //another approach---
+        if(dist <= tank) return 0;
+        if(stops == null || stops.length == 0) return -1;
+        Arrays.sort(stops);
+        if(stops[stops.length - 1]+tank < dist || stops[0] > tank) return -1;
+        int refuels = 0;
+        int prev = 0, curr = 0;
+        for(int i = 0; i < stops.length; i++) {
+            if(stops[i] <= tank+curr) {
+                prev = stops[i];
+            } else {
+                if(curr == prev) return -1;
+                curr = prev;
+                refuels++;
+                if(curr+tank >= dist) return refuels;
+                i--;
             }
         }
-        return count;
+        return refuels+1;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int dist = scanner.nextInt();
         int tank = scanner.nextInt();
         int n = scanner.nextInt();
-        int stops[] = new int[n+2];
-        stops[0]=0;
-        for (int i = 1; i < n+1; i++) {
+        int stops[] = new int[n];
+        for (int i = 0; i < n; i++) {
             stops[i] = scanner.nextInt();
         }
-        stops[n+1] = dist;
-        /*
-        for (int item:stops) {
-            System.out.print(item+" ");
-        }
-        System.out.println();
-         */
-        if(n==1){
-            System.out.println(1);
-        }else{
-            System.out.println(computeMinRefills(dist, tank, stops));
-        }
+        System.out.println(computeMinRefills(dist, tank, stops));
     }
 }
